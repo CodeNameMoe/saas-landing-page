7d718f557786e4eb809aa0fe48686d4aa3530e96
codenamemoe <newmoeali@gmail.com>
2024-02-25 16:29:29 +0000

    faq section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Faq from "./components/Faq";
 
 export default function Home() {
 	return (
@@ -10,6 +11,7 @@ export default function Home() {
 			<Hero />
 			<div className="px-[20px] lg:container lg:px-20 mx-auto">
 				<Features />
+				<Faq />
 			</div>
 		</div>
 	);
